224b1b5e85ee81ef07bdf5bc0a98b22cf0dcb221
SetayeshAmini <162023047+SetayeshAmini@users.noreply.github.com>
2026-07-09 23:07:12 +0430

    I do Project section

---
+++ Project.py
@@ -0,0 +1,21 @@
+from Assesment import Assesment
+class Project(Assesment):
+
+    def __init__(self, title, max_score):
+        super().__init__(title, max_score)
+
+    def display_info(self):
+        print(f"Project: {self.title} - Max Score: {self.max_score}")
+
+    def grade_msssage(self, score):
+        percentage = self.calculate_percentage(score)
+        if percentage >= 95:
+            return "Grade: A+"
+        elif percentage >= 80:
+            return "Grade: A"
+        elif percentage >= 70:
+            return "Grade: B"
+        elif percentage >= 50:
+            return "Grade: C"
+        else:
+            return "Needs more practice"
