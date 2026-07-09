180284d459e7d6df32f46bc5f8ff1ab3f106dd7e
SetayeshAmini <162023047+SetayeshAmini@users.noreply.github.com>
2026-07-09 22:07:42 +0430

    I can add assessments and record grades.

---
+++ Assesment.py
@@ -0,0 +1,20 @@
+class Assesment:
+    def __init__(self, title, max_score):
+        self.title = title
+        self.max_score = max_score
+
+    def calculate_percentage(self, score):
+        if self.max_score == 0:
+            return 0.0
+        return (score / self.max_score) * 100
+
+    def grade_msssage(self, score):
+        percentage = self.calculate_percentage(score)
+        if percentage >= 50:
+            return f"Exam passed! result for {self.title} is {percentage:.1f}%"
+        else:
+            return f"Exam failed! result for {self.title} is {percentage:.1f}%"
+
+    def display_info(self):
+        print(f"{self.title} - Max score: {self.max_score}.")
+
