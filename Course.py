0f4047975478fcd0cb0d734118a9df940ee9fa4b
SetayeshAmini <162023047+SetayeshAmini@users.noreply.github.com>
2026-07-09 23:35:04 +0430

    I can add courses and enroll students.

---
+++ Course.py
@@ -0,0 +1,30 @@
+class Course:
+    def __init__(self, course_code, course_name):
+        self.course_code = course_code
+        self.course_name = course_name
+
+        self.students = []
+        self.assessments = []
+
+    def add_student(self, student_id):
+        if student_id not in self.students:
+            self.students.append(student_id)
+
+    def add_assessment(self, assessment_):
+        self.assessments.append(assessment_)
+
+    def find_assessment(self, title):
+        for assessment in self.assessments:
+            if assessment.title == title:
+                return assessment
+        return None
+
+    def display_info(self):
+        print(f"Course Code: {self.course_code}")
+        print(f"Course Name: {self.course_name}")
+        print(f"Enrolled Students: {len(self.students)}")
+
+        if self.assessments:
+            print("\nAssessments:")
+            for assessment in self.assessments:
+              print(f"- {assessment.title} / Max score: {assessment.max_score}")
