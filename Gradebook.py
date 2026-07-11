57dda16e026ba8584a7ee245b3db64468705e1da
SetayeshAmini <162023047+SetayeshAmini@users.noreply.github.com>
2026-07-11 23:15:09 +0430

    recode grade and calculate average  are done in gradebook.

--- Gradebook.py
+++ Gradebook.py
@@ -20,7 +20,36 @@ class Gradebook:
               self.grades[student_id] = {}
            if course_code not in self.grades[student_id]:
              self.grades[student_id][course_code] = {}
-           print(f"Student {student_id} enrolled in course {course_code}")
+           print(f"Student {student_id} enrolled in course {course_code}.")
        else:
            print("Error: student or course not found")
 
+    def add_assessment(self, assessment, course_code):
+        if course_code in self.courses:
+            self.courses[course_code].assessments.append(assessment)
+            print(f"Assessment {assessment.title} added to course {course_code}.")
+        else:
+            print("Error: course not found.")
+
+    def record_grade(self, student_id, course_code, assessment_title, score):
+        if student_id in self.grades and course_code in self.grades[student_id]:
+            course = self.courses[course_code]
+            valid_titles = [asm.title for asm in course.assessments]
+
+            if assessment_title in valid_titles:
+                self.grades[student_id][course_code][assessment_title] = score
+                print(f"Grade {score} recorded for {assessment_title} in course {course_code}.")
+            else:
+                print("Error: This assessment is not defined for this course.")
+        else:
+            print("Error: Invalid student or course enrollment.")
+
+    def calculate_average(self, student_id, course_code):
+        if student_id in self.grades and course_code in self.grades[student_id]:
+            student_course_grades = self.grades[student_id][course_code]
+            if not student_course_grades:
+                return 0
+            total = sum(student_course_grades.values())
+            count = len(student_course_grades)
+            return total / count
+        return 0
